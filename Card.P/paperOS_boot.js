2f9dbed72e534fa9dc21ef9b4e228fbc11f45239
mcbutnugget <115441757+mcbutnugget@users.noreply.github.com>
2024-03-22 13:32:47 -0800

    Add files via upload

---
+++ Card.P/paperOS_boot.js
@@ -0,0 +1,43 @@
+var computator = [
+    ["p",[
+    ["main",[
+      ["..",[]]
+    ]],
+    ["appData",[
+        ["Stickynote",[
+            ["stickynote.incd",`punctuation: rgb(117, 95, 224)\nkeyword: #c5b6e0\nstring: #36a2aa\nnumber: rgb(99, 166, 220)\nboolean: #9696ff\nregex: rgb(255, 115, 115)\nclass-name: rgb(212, 0, 255)\noperator: rgb(0, 255, 217)\nfunction: rgb(88, 105, 201)\nconstant: rgb(216, 104, 222)\ncomment: #5e5e5e\ntext: rgb(124, 224, 147)`]
+        ]],
+        ["paperRuntime",[
+            ["papdat.incd",`pap_start_data:true\npap_hello:true\npap_controller:false\npap_counter:false`]
+        ]]
+    ]],
+    ["sys",[
+        ["data.incd",`welcome:true\nwarn:true\nstartPath:p/main\n`],
+        ["functions.js",readDisk("./functions functions.js")]
+    ]]
+    ["tmp",[
+        ['prgm',[
+            ["Stickynote",[
+                ["asdjfiobklegnmojwnmvjkdjwaelojvdmkav",[
+                    ["titlebar","false"],
+                    ["title","stickynote:asdf.incd"],
+                    ["backgroundImg","p/appData/Stickynote/images/woa.png"],
+                    ["backgroundCol","none"],
+                    ["resizable","false"],
+                    ["draggable","true"]
+                ]],
+                ["adsjimoklasfjviomdfklsajmfvoikdlajiok",[
+                    ["titlebar","false"],
+                    ["title","stickynote:asdf.incd"],
+                    ["backgroundImg","p/appData/Stickynote/images/woa.png"],
+                    ["backgroundCol","none"],
+                    ["resizable","false"],
+                    ["draggable","true"]
+                ]]
+            ]]
+        ]]
+    ]]
+    ] 
+  ]
+    
+  ]
